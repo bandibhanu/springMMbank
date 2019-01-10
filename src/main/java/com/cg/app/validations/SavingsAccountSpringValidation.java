a88630e45336d599973d50683c65207fb0bf7f56
Bhanu Prakash Reddy <bandibhanu97@gmail.com>
2019-01-10 10:13:44 +0530

    spring

--- src/main/java/com/cg/app/validations/SavingsAccountSpringValidation.java
+++ src/main/java/com/cg/app/validations/SavingsAccountSpringValidation.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import com.cg.app.account.SavingsAccount;
 import com.moneymoney.exception.InsufficientFundsException;
 
 @Aspect
@@ -17,17 +18,33 @@ public class SavingsAccountSpringValidation {
 
 	@Around("execution(* com.cg.app.account.service.SavingsAccountServiceImpl.deposit(..))")
 	public void servingsAccountDepositeValidation(ProceedingJoinPoint pjp) throws Throwable {
-		logger.info("6534534");
-		logger.info("Function Name is : " + pjp.getSignature());
+		logger.info("DepositeValidation");
 		Object[] params = pjp.getArgs();
-		System.out.println("dtfgdfgdf");
-		double amount = (Double) params[0];
-		if (amount > 0) {
-			pjp.proceed();
-		} else {
-			throw new InsufficientFundsException("Invalid Input or Insufficinet funds!!!");
-		}
+		SavingsAccount savingsAccount = (SavingsAccount) params[0];
+		logger.info("Function Name is : " + pjp.getSignature());
 
+		double amount = (Double) params[1];
+		if (amount > 0 && savingsAccount != null) {
+			pjp.proceed();
+		} else if (savingsAccount == null) {
+			logger.info("account number not existed");
+		} else {
+			logger.info("Deposit amount should be positive");
+		}
 	}
 
+	@Around("executions(*com.cg.app.account.service.SavingsAccountServiceImpl.withdraw(..))")
+	public void servingsAccountWithdrawValidation(ProceedingJoinPoint pjp) throws Throwable {
+
+		Object[] params = pjp.getArgs();
+		SavingsAccount savingsAccount = (SavingsAccount) params[0];
+		if (savingsAccount != null) {
+			double currentBalance = savingsAccount.getBankAccount().getAccountBalance();
+			double amount = (Double) params[1];
+			if (amount > 0 && currentBalance >= amount) {
+				pjp.proceed();
+			}
+
+		}
+	}
 }
